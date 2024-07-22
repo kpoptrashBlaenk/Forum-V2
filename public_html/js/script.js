c8a796d2a546997a21d465ce7203224a1edd08c0
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-07-22 14:26:56 +0200

    Link Categories on Slam

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
     //LIKE & DISLIKE
     const likeButton = $('#likePostButton');
 
--- views/slams/show.view.php
+++ views/slams/show.view.php
@@ -21,7 +21,7 @@ require basePath('views/partials/header.php');
         <div class="row mb-3">
             <div>
                 <a class="fw-bold mb-0 text-secondary link-underline link-underline-opacity-0"
-                   href="/profile?user=<?= htmlspecialchars($post['username']) ?>&page=1">
+                   href="/profile?user=<?= htmlspecialchars($post['username']) ?>">
                     <?= htmlspecialchars($post['username']) ?>
                 </a>
             </div>
@@ -36,23 +36,24 @@ require basePath('views/partials/header.php');
         </div>
 
         <form id="likePostForm">
-                <input type="hidden" name="post_id" value="<?= $post['id'] ?>"
-                <div class="row mt-4">
-                    <div class="col-md-8">
-                        <?php if ($post['user_has_liked'] > 0) : ?>
-                            <button type="submit" class="btn btn-sm rounded-pill btn-secondary me-2" id="likePostButton">
-                                Unlike: <?= $post['num_likes'] ?></button>
-                        <?php else : ?>
-                            <button type="submit" class="btn btn-sm btn-danger rounded-pill me-2 border border-dark" id="likePostButton">
-                                Like: <?= $post['num_likes'] ?></button>
-                        <?php endif; ?>
-                        <span class="badge border border-dark text-dark fs-6">
-                        <img src="<?= resources($post['image_url']) ?>" alt="SQL" width="32" height="32" class="bi">
+            <input type="hidden" name="post_id" value="<?= $post['id'] ?>"
+            <div class="row mt-4">
+                <div class="col-md-8">
+                    <?php if ($post['user_has_liked'] > 0) : ?>
+                        <button type="submit" class="btn btn-sm rounded-pill btn-secondary me-2" id="likePostButton">
+                            Unlike: <?= $post['num_likes'] ?></button>
+                    <?php else : ?>
+                        <button type="submit" class="btn btn-sm btn-danger rounded-pill me-2 border border-dark"
+                                id="likePostButton">
+                            Like: <?= $post['num_likes'] ?></button>
+                    <?php endif; ?>
+                    <a class="badge border border-dark text-dark fs-6 nav-link" href="/slams?category=<?= $post['category_id'] ?>">
+                        <img src="<?= resources($post['image_url']) ?>" alt="<?= $post['name'] ?>" width="32" height="32" class="bi">
                         <?= $post['name'] ?>
-                    </span>
-                    </div>
+                    </a>
                 </div>
-            </form>
+            </div>
+        </form>
     </div>
 </div>
 
@@ -78,7 +79,7 @@ require basePath('views/partials/header.php');
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-10">
-            <h3 id="commentsCount" >Comments: <?= count($comments) ?></h3>
+            <h3 id="commentsCount">Comments: <?= count($comments) ?></h3>
             <?php if (count($comments) === 0) : ?>
                 <h1 class="my-5 text-secondary">Wow, so empty... :3</h1>
             <?php else : ?>
@@ -86,15 +87,16 @@ require basePath('views/partials/header.php');
                     <div id="commentsContainer" class="card mb-3">
                         <div class="card-body">
 
-                                <div class="position-absolute top-0 end-0 mt-2 me-2">
-                                    <form action="slam/comment" method="POST" class="deleteCommentForm">
-                                        <input type="hidden" name="comment_id" value="<?= $comment['id'] ?>">
-                                        <input type="hidden" name="post_id" value="<?= $post['id'] ?>">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <button type="button" class="btn btn-sm btn-primary me-2 editCommentButton">Edit</button>
-                                        <button type="submit" class="btn btn-sm btn-danger me-2">Delete</button>
-                                    </form>
-                                </div>
+                            <div class="position-absolute top-0 end-0 mt-2 me-2">
+                                <form action="slam/comment" method="POST" class="deleteCommentForm">
+                                    <input type="hidden" name="comment_id" value="<?= $comment['id'] ?>">
+                                    <input type="hidden" name="post_id" value="<?= $post['id'] ?>">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button type="button" class="btn btn-sm btn-primary me-2 editCommentButton">Edit
+                                    </button>
+                                    <button type="submit" class="btn btn-sm btn-danger me-2">Delete</button>
+                                </form>
+                            </div>
                             <?php if (isset($_SESSION['user']['user_id']) && $_SESSION['user']['user_id'] === $comment['user_id']) : ?>
                             <?php endif; ?>
                             <h5 class="card-title"><?= htmlspecialchars($comment['username']) ?></h5>
